a099644eac155fa62b04e35c7de6fa2bae0ea1ea
Silasprata23 <silasprata23@gmail.com>
2026-08-13 18:28:33 -0300

    feat:folder structure

---
+++ src/style/theme.ts
@@ -0,0 +1,13 @@
+
+export default {
+    colors: {
+        skyBlue: 'C8D9E6',
+        navy: '2F4156',
+        teal: '567C8D',
+        beige: 'F5EFEB',
+        white: 'FFFFFF',
+        black: '1A1A1A',
+        dustyGray: '878787',
+
+    }
+}
